1c1b1284343303c139a6b8578fa1408fc6f275a1
Scott Sperling <scsper@gmail.com>
2013-06-22 15:38:56 -0700

    canvas no longer has horizontal scrollbar

--- js/canvas.js
+++ js/canvas.js
@@ -1,5 +1,5 @@
 function Canvas(selector) {
-    this.canvas = document.getElementById(selector);
+    this.selector = selector;
 }
 
 Canvas.prototype = {
@@ -10,28 +10,25 @@ Canvas.prototype = {
 
     bind: function() {
         // resize the canvas to fill browser window dynamically
-        window.addEventListener('resize', this.resize, false);
+        window.addEventListener('resize', this.resize.bind(this), false);
     },
 
     resize: function() {
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight - document.getElementsByClassName("main")[0];
+        this.canvas = document.getElementById(this.selector);
+        this.canvas.width = document.getElementsByClassName("main")[0].offsetWidth;
+        this.canvas.height = window.innerHeight - document.getElementsByClassName("main")[0].offsetHeight - document.getElementsByClassName("pure-menu")[0].offsetHeight;
 
-        console.log("width: " + this.canvas.width);
-        console.log("height: " + this.canvas.height);
         this.draw();
     },
 
     draw: function() {
-        this.drawLine([0, 50], [50, 50]);
+        var height = this.canvas.height / 2;
+        this.drawLine([0, height], [this.canvas.width, height]);
     },
 
     drawLine: function(start, end) {
         var canvas = this.canvas,
             ctx = canvas.getContext("2d");
-
-        c.width = window.width;
-        c.height = 100;
 
         ctx.translate(.5, .5);
 
