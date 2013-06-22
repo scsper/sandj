34c07cfca7347c988dfcca373cca9cd25e8f0e77
Jess <itsjesschen@gmail.com>
2013-06-22 15:18:13 -0700

    updated details

--- js/details.js
+++ js/details.js
@@ -1,13 +1,39 @@
 // http://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&sensor=false
-window.onload = initMap;
+// window.onload = Map.init;
 
-function initMap(){
-	var mapOptions = {
-		center: new google.maps.LatLng(-34, 150 ),
-		zoom: 8,
-		mapTypeId: google.maps.mapTypeId.ROADMAP
-	};
-	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+function Map(){
+	this.directionService = new google.maps.DirectionsService();
 }
-google.maps.event.addDomListener(window, 'load', initMap);
+Map.prototype = {
+	init: function (){
+		var mapOptions = {
+	        center: new google.maps.LatLng(-34.397, 150.644),
+	        zoom: 8,
+	        mapTypeId: google.maps.MapTypeId.ROADMAP
+	    };
+	    this.map = new google.maps.Map(document.getElementById("map-canvas"),
+        	mapOptions);
+	},
+	initDirector : function(){
+		this.directionsDisplay = new google.maps.DirectionsRenderer();
+		this.directionsDisplay.setMap(this.map);
+	},
+	calcRoute : function(){
+		var start = "Orange County";
+		var end ="Bakersfield";
+		var request = {
+			origin: start,
+			destination: end,
+			travelMode:google.maps.TravelMode.DRIVING
+		};
+		var directionsDisplay = this.directionsDisplay;
+		this.directionService.route(request, function(result, status){
+			if (status == google.maps.DirectionsStatus.OK) {
+     			directionsDisplay.setDirections(result);
+    		}
+		});
+	}
+}
+
+//google.maps.event.addDomListener(window, 'load', initMap);
 
