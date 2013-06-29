dfc89b2de5883b6277c8fe9c9ef93e76c5a7ae86
Jess <itsjesschen@gmail.com>
2013-06-29 16:03:05 -0700

    RSVP is centered, animations and critters working correctly

--- js/rsvp.js
+++ js/rsvp.js
@@ -2,16 +2,19 @@ var MAX_NUMBER_GUESTS = 5;
 
 function changedNum(){
 	var selectBox = document.getElementById("number");
-	var number = selectBox.options[selectBox.selectedIndex].text;
+	var number = selectBox.options[selectBox.selectedIndex].value;
+	var critters = document.getElementsByClassName('roll');
+	console.log(critters);
 	// var $critters = $(" li.roll" );
 	var i = 1;
 
-	// while(i < MAX_NUMBER_GUESTS){
+	while(i < MAX_NUMBER_GUESTS){
 
-	// 	if(i < number){
-			
-	// 	}else{
-
-	// 	}
-	// }
+		if(i < number){
+			critters[i-1].style.display = "inline-block";
+		}else{
+			critters[i-1].style.display = "none";
+		}
+		i++;
+	}
 }
