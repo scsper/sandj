56fe7770b3443d182567a48ad3760877ddbbc147
Scott Sperling <ssperlin@yahoo-inc.com>
2013-12-26 23:25:24 -0800

    add guests to the database

--- php/guest.php
+++ php/guest.php
@@ -1,17 +1,39 @@
 <?php
-class Guest {
-    public $firstName;
-    public $lastName;
-    public $email;
-    public $status;
-    public $familyId;
 
-    function __construct($firstName, $lastName, $email, $status, $familyId) {
-        $this->firstName = $firstName;
-        $this->lastName = $lastName;
-        $this->email = $email;
-        $this->status = $status;
+require 'connect.php';
+
+class Guest {
+    private $name;
+    private $status;
+    private $familyId;
+    private $code;
+
+    public function __construct($firstName, $lastName, $familyId, $rsvp) {
+        $this->name = $firstName . " " . $lastName;
+        $this->rsvp = $rsvp;
         $this->familyId = $familyId;
+    }
+
+    public function display() {
+        echo $this->firstName;
+        echo $this->lastName;
+        echo $this->rsvp;
+        echo $this->familyId;
+        echo $this->code;
+        echo '<br/>';
+    }
+
+    public function set_code($code) {
+        $this->code = $code;
+    }
+
+    public function add() {
+        $sql = "INSERT INTO guest (name, rsvp_id, food_id, family_id, code) VALUES (\"" . $this->name . "\", " . $this->rsvp . ", 4, " . $this->familyId . ", \"" . $this->code . "\")";
+        $result = mysql_query($sql);
+
+        if (!$result) {
+            die('Invalid query: ' . mysql_error());
+        }
     }
 }
 
--- php/read_csv.php
+++ php/read_csv.php
@@ -1,22 +1,45 @@
 <?php
 
-function readCSV() {
+require 'guest.php';
+require 'connect.php';
+
+function get_guests() {
     $row = 1;
     $filename = "../assets/guestlist.csv";
+    $guests = array();
 
     if (($handle = fopen($filename, "r")) !== FALSE) {
         while (($data = fgetcsv($handle)) !== FALSE) {
             $num = count($data);
-            echo "<p> $num fields in line $row: <br /></p>\n";
+            // echo "<p> $num fields in line $row: <br /></p>\n";
             $row++;
-            for ($c=0; $c < $num; $c++) {
-                echo $data[$c] . "<br />\n";
-            }
+
+            $guest = new Guest($data[0], $data[1], $data[2], $data[3]);
+            $guest->set_code(get_unique_code());
+            array_push($guests, $guest);
         }
         fclose($handle);
     }
+
+    return $guests;
 }
 
-readCSV();
+function get_unique_code() {
+    $code = md5(uniqid(rand(), true));
+    $code = substr($code, -6);
+    return $code;
+}
+
+/* Uncomment lines to add guests back into the database */
+
+// function add_to_db($guests) {
+//     for($i = 0; $i < count($guests); $i++) {
+//         $guests[$i]->add();
+//     }
+// }
+
+// $guests = get_guests();
+// add_to_db($guests);
+// echo 'guests added to db';
 
 ?>
